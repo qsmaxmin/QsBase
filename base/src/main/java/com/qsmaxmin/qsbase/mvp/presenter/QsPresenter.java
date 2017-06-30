cf5a0a4dfaca180e8050fb8880c6bb87ba980a72
qsmaxmin <17600662013@163.com>
2017-07-01 00:19:44 +0800

    框架开发

--- base/src/main/java/com/qsmaxmin/qsbase/mvp/presenter/QsPresenter.java
+++ base/src/main/java/com/qsmaxmin/qsbase/mvp/presenter/QsPresenter.java
@@ -15,7 +15,7 @@ public class QsPresenter<V extends QsIView> {
     private boolean isAttach;
     private V       mView;
 
-    public String getTag() {
+    public String initTag() {
         return getClass().getSimpleName();
     }
 
@@ -46,7 +46,7 @@ public class QsPresenter<V extends QsIView> {
      * 自定义异常处理
      */
     public final void methodError(QsException exception) {
-        L.e(getTag(), "methodError ：" + exception.getMessage());
+        L.e(initTag(), "methodError ：" + exception.getMessage());
         if (mView != null) {
             if (mView.isOpenViewState()) {
                 mView.showErrorView();
