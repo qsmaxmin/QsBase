c7f386bdbe93d9d3d85d87cfa69f926a241a3d73
qsmaxmin <17600662013@163.com>
2017-07-03 18:28:20 +0800

    框架快速开发

---
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsDialogFragment.java
@@ -0,0 +1,45 @@
+package com.qsmaxmin.qsbase.common.widget.dialog;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by sky on 15/2/28. dialog 基类
+ */
+public abstract class QsDialogFragment extends DialogFragment {
+
+    @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, getDialogTheme());
+    }
+
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getDialog().setCanceledOnTouchOutside(true);
+        final Window window = getDialog().getWindow();
+        if (window != null) {
+            window.setBackgroundDrawableResource(android.R.color.transparent);
+            window.getDecorView().setPadding(0, 0, 0, 0);
+            WindowManager.LayoutParams params = window.getAttributes();
+            setAttribute(params);
+            window.setAttributes(params);
+        }
+        return inflater.inflate(dialogLayoutId(), container);
+    }
+
+    protected void setAttribute(WindowManager.LayoutParams params) {
+        params.gravity = Gravity.BOTTOM;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+    }
+
+    protected abstract int dialogLayoutId();
+
+    protected abstract int getDialogTheme();
+}
