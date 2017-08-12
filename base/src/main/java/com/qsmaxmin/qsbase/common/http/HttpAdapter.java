9a53179049dd85d85033e0ab9bcabde74e77deb3
程亮 <chengliang@grapedu.cn>
2017-08-12 12:12:26 +0800

    HttpAdapter优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpAdapter.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpAdapter.java
@@ -135,14 +135,16 @@ public class HttpAdapter {
         if (TextUtils.isEmpty(url)) throw new QsException(QsExceptionType.UNEXPECTED, "url error... method:" + method.getName() + "  request url is null...");
         Annotation[][] parameterAnnotations = method.getParameterAnnotations();
         Map<String, Object> params = null;
-        for (int i = 0; i < parameterAnnotations.length; i++) {
-            Annotation[] annotations = parameterAnnotations[i];
-            for (Annotation annotation : annotations) {
-                if (annotation instanceof Query && args != null && i < args.length) {
-                    if (params == null) params = new HashMap<>();
-                    Object arg = args[i];
-                    String key = ((Query) annotation).value();
-                    params.put(key, arg);
+        if (parameterAnnotations != null) {
+            for (int i = 0; i < parameterAnnotations.length; i++) {
+                Annotation[] annotations = parameterAnnotations[i];
+                for (Annotation annotation : annotations) {
+                    if (annotation instanceof Query && args != null && i < args.length) {
+                        if (params == null) params = new HashMap<>();
+                        Object arg = args[i];
+                        String key = ((Query) annotation).value();
+                        params.put(key, arg);
+                    }
                 }
             }
         }
@@ -152,7 +154,6 @@ public class HttpAdapter {
                 url.append("&").append(key).append("=").append(object);
             }
         }
-
         Request.Builder requestBuilder = new Request.Builder();
         requestBuilder.headers(httpBuilder.getHeaderBuilder().build());
         Request request = requestBuilder.tag(url.toString()).url(url.toString()).method("GET", null).build();
@@ -174,18 +175,20 @@ public class HttpAdapter {
         Annotation[][] parameterAnnotations = method.getParameterAnnotations();//参数可以有多个注解
         Object body = null;
         HashMap<String, Object> params = null;
-        for (int i = 0; i < parameterAnnotations.length; i++) {
-            Annotation[] annotations = parameterAnnotations[i];
-            for (Annotation annotation : annotations) {
-                if (args != null && i < args.length) {
-                    if (annotation instanceof Body) {
-                        body = args[i];
-                        break;
-                    } else if (annotation instanceof Query) {
-                        if (params == null) params = new HashMap<>();
-                        Object arg = args[i];
-                        String key = ((Query) annotation).value();
-                        params.put(key, arg);
+        if (parameterAnnotations != null) {
+            for (int i = 0; i < parameterAnnotations.length; i++) {
+                Annotation[] annotations = parameterAnnotations[i];
+                for (Annotation annotation : annotations) {
+                    if (args != null && i < args.length) {
+                        if (annotation instanceof Body) {
+                            body = args[i];
+                            break;
+                        } else if (annotation instanceof Query) {
+                            if (params == null) params = new HashMap<>();
+                            Object arg = args[i];
+                            String key = ((Query) annotation).value();
+                            params.put(key, arg);
+                        }
                     }
                 }
             }
@@ -250,32 +253,34 @@ public class HttpAdapter {
             throw new QsException(QsExceptionType.UNEXPECTED, "url path error... method:" + method.getName() + "  path=" + path + "  (path is not start with '/')");
         }
         Annotation[][] annotations = method.getParameterAnnotations();
-        if (annotations.length != args.length) {
-            throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + " params have to have one annotation, such as @Query @Path");
-        }
-        for (int i = 0; i < annotations.length; i++) {
-            Annotation[] annotationArr = annotations[i];
-            if (annotationArr.length != 1) {
-                throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + " params have to have one annotation, but there is more than one !");
+        if (annotations != null && args != null && annotations.length > 0 && args.length > 0) {
+            if (annotations.length != args.length) {
+                throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + "  params have to have one annotation, such as @Query @Path");
             }
-            Annotation ann = annotationArr[0];
-            if (ann instanceof Path) {
-                StringBuilder stringBuilder = new StringBuilder();
-                String[] split = path.split(PATH_REPLACE);
-                Object arg = args[i];
-                if (!(arg instanceof String[])) {
-                    throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + " @Path annotation only fix String[] arg !");
+            for (int i = 0; i < annotations.length; i++) {
+                Annotation[] annotationArr = annotations[i];
+                if (annotationArr.length != 1) {
+                    throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + "  params have to have one annotation, but there is more than one !");
                 }
-                String[] param = (String[]) arg;
-                if (split.length - param.length > 1) {
-                    throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + "the path with '{xx}' is more than @Path annotation arg length!");
-                }
-                for (int index = 0; index < split.length; index++) {
-                    if (index < param.length) {
-                        stringBuilder.append(split[index]).append(param[index]);
+                Annotation ann = annotationArr[0];
+                if (ann instanceof Path) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String[] split = path.split(PATH_REPLACE);
+                    Object arg = args[i];
+                    if (!(arg instanceof String[])) {
+                        throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + "  @Path annotation only fix String[] arg !");
                     }
+                    String[] param = (String[]) arg;
+                    if (split.length - param.length > 1) {
+                        throw new QsException(QsExceptionType.UNEXPECTED, "params error method:" + method.getName() + "  the path with '{xx}' is more than @Path annotation arg length!");
+                    }
+                    for (int index = 0; index < split.length; index++) {
+                        if (index < param.length) {
+                            stringBuilder.append(split[index]).append(param[index]);
+                        }
+                    }
+                    path = stringBuilder.toString();
                 }
-                path = stringBuilder.toString();
             }
         }
         StringBuilder url = new StringBuilder(terminal);
