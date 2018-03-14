d370a04b83df0cc316a6138d833eafb8be769078
qsmaxmin <17600662013@163.com>
2018-03-14 11:15:42 +0800

    HttpAdapter请求体支持Form提交

--- base/src/main/java/com/qsmaxmin/qsbase/common/aspect/FormBody.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/aspect/FormBody.java
@@ -3,7 +3,6 @@ package com.qsmaxmin.qsbase.common.aspect;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import java.util.HashMap;
 
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -11,7 +10,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * @CreateBy qsmaxmin
  * @Date 2017/6/29 15:12
- * @Description 表单参数注解
+ * @Description 表单参数注解，支持Map、String（格式：key1=10&key2=1.22&key3=hollow）和Object类型，Object类型会反射获取字段名和值
  */
 @Documented
 @Target(PARAMETER)
