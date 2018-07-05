f81efc1430d2249a8d9c70710133eb9664b25016
qsmaxmin <17600662013@163.com>
2018-07-05 15:55:23 +0800

    ScreenHelper新增api

--- base/src/main/java/com/qsmaxmin/qsbase/common/utils/ScreenHelper.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/utils/ScreenHelper.java
@@ -4,6 +4,7 @@ import android.support.v4.app.FragmentActivity;
 
 import com.qsmaxmin.qsbase.common.log.L;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 
@@ -27,8 +28,8 @@ public final class ScreenHelper {
     /**
      * FragmentActivity堆栈 单例模式
      */
-    private static final Stack<FragmentActivity> fragmentActivities = new Stack<>();
-
+    private final Stack<FragmentActivity> fragmentActivities = new Stack<>();
+    private ArrayList<OnTaskChangeListener> listeners;
 
     static ScreenHelper getInstance() {
         return instance;
@@ -51,6 +52,7 @@ public final class ScreenHelper {
     public void pushActivity(FragmentActivity activity) {
         if (activity != null) {
             fragmentActivities.add(activity);
+            onActivityCallback(activity);
             L.i(TAG, "activity入栈:" + activity.getClass().getSimpleName() + "   当前栈大小：" + fragmentActivities.size());
         } else {
             L.e(TAG, "pushActivity 传入的参数为空!");
@@ -64,6 +66,7 @@ public final class ScreenHelper {
         if (activity != null) {
             activity.finish();
             fragmentActivities.remove(activity);
+            onActivityRemoveCallback(activity);
             L.i(TAG, "activity出栈:" + activity.getClass().getSimpleName() + "   当前栈大小：" + fragmentActivities.size());
         } else {
             L.e(TAG, "popActivity 传入的参数为空!");
@@ -71,6 +74,7 @@ public final class ScreenHelper {
         if (fragmentActivities.size() <= 0) {
             QsHelper.getInstance().getImageHelper().clearMemoryCache();
             QsHelper.getInstance().getThreadHelper().shutdown();
+            if (listeners != null) listeners.clear();
         }
     }
 
@@ -90,5 +94,40 @@ public final class ScreenHelper {
 
     public Stack<FragmentActivity> getActivityStack() {
         return fragmentActivities;
+    }
+
+    public void addOnTaskChangedListener(OnTaskChangeListener listener) {
+        if (listener == null) return;
+        if (listeners == null) listeners = new ArrayList<>(3);
+        if (!listeners.contains(listener)) listeners.add(listener);
+    }
+
+    public void removeOnTaskChangedListener(OnTaskChangeListener listener) {
+        if (listener == null) return;
+        if (listeners != null) {
+            listeners.remove(listener);
+        }
+    }
+
+    public interface OnTaskChangeListener {
+        void onActivityAdd(FragmentActivity activity);
+
+        void onActivityRemove(FragmentActivity activity);
+    }
+
+    private void onActivityCallback(FragmentActivity activity) {
+        if (listeners != null) {
+            for (OnTaskChangeListener listener : listeners) {
+                listener.onActivityAdd(activity);
+            }
+        }
+    }
+
+    private void onActivityRemoveCallback(FragmentActivity activity) {
+        if (listeners != null) {
+            for (OnTaskChangeListener listener : listeners) {
+                listener.onActivityRemove(activity);
+            }
+        }
     }
 }
