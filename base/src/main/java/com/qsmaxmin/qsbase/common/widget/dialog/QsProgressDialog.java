f5e26352f7897b8dc275661b5f3e9f8251a74944
qsmaxmin <17600662013@163.com>
2018-07-06 10:08:34 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsProgressDialog.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsProgressDialog.java
@@ -3,6 +3,8 @@ package com.qsmaxmin.qsbase.common.widget.dialog;
 import android.view.Gravity;
 import android.view.WindowManager;
 
+import com.qsmaxmin.qsbase.common.log.L;
+
 /**
  * @CreateBy qsmaxmin
  * @Date 17/8/3  上午12:35
@@ -34,10 +36,12 @@ public abstract class QsProgressDialog extends QsDialogFragment {
 
     public void setIsShowing(boolean show) {
         this.isShow = show;
+        L.i(initTag(), "setIsShowing....... showing:true");
     }
 
     @Override public void onDetach() {
         super.onDetach();
         setIsShowing(false);
+        L.i(initTag(), "onDetach....... set is showing:false");
     }
 }
