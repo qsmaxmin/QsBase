b0cac0a2ee3c6905542de47f609ef169af497714
qsmaxmin <17600662013@163.com>
2018-07-23 11:08:01 +0800

    PresenterUtils优化

--- base/src/main/java/com/qsmaxmin/qsbase/mvp/presenter/Presenter.java
+++ base/src/main/java/com/qsmaxmin/qsbase/mvp/presenter/Presenter.java
@@ -16,5 +16,5 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target(TYPE)
 @Retention(RUNTIME)
 public @interface Presenter {
-    Class value() default QsPresenter.class;
+    Class value();
 }
