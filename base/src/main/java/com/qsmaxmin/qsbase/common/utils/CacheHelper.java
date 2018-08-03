1dc9db08184ffc26942b2cd97621314397f7182e
qsmaxmin <17600662013@163.com>
2018-08-03 13:59:38 +0800

    CacheHelper新增api

--- base/src/main/java/com/qsmaxmin/qsbase/common/utils/CacheHelper.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/utils/CacheHelper.java
@@ -4,11 +4,15 @@ package com.qsmaxmin.qsbase.common.utils;
 import android.content.Context;
 
 import com.google.gson.Gson;
+import com.qsmaxmin.qsbase.common.log.L;
 import com.qsmaxmin.qsbase.common.model.QsModel;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 
@@ -22,12 +26,12 @@ public class CacheHelper {
     CacheHelper() {
     }
 
-    public <T extends QsModel> void saveObject2File(T model, String fileName) {
+    public <T extends QsModel> void saveObject2File(T model, String key) {
         Gson gson = new Gson();
         String json = gson.toJson(model);
         FileOutputStream fos = null;
         try {
-            fos = QsHelper.getInstance().getApplication().openFileOutput(fileName, Context.MODE_PRIVATE);
+            fos = QsHelper.getInstance().getApplication().openFileOutput(key, Context.MODE_PRIVATE);
             fos.write(json.getBytes());
         } catch (Exception e) {
             e.printStackTrace();
@@ -36,18 +40,67 @@ public class CacheHelper {
         }
     }
 
-    public <T extends QsModel> T getObjectFromFile(String fileName, Class<T> clazz) {
+    public <T extends QsModel> void saveObject2File(T model, File cacheFile) {
+        if (cacheFile == null) return;
+        File parentFile = cacheFile.getParentFile();
+        if (!parentFile.exists()) {
+            boolean mkdirs = parentFile.mkdirs();
+        } else if (cacheFile.exists()) {
+            boolean delete = cacheFile.delete();
+        }
+        Gson gson = new Gson();
+        String json = gson.toJson(model);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(cacheFile);
+            fos.write(json.getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            QsHelper.getInstance().closeStream(fos);
+        }
+    }
+
+    public <T extends QsModel> T getObjectFromFile(String key, Class<T> clazz) {
         FileInputStream fileInputStream = null;
         InputStreamReader inputStreamReader = null;
         BufferedReader bufferedReader = null;
         try {
-            fileInputStream = QsHelper.getInstance().getApplication().openFileInput(fileName);
+            fileInputStream = QsHelper.getInstance().getApplication().openFileInput(key);
             inputStreamReader = new InputStreamReader(fileInputStream);
             bufferedReader = new BufferedReader(inputStreamReader);
             String json = bufferedReader.readLine();
             Gson gson = new Gson();
             return gson.fromJson(json, clazz);
+        } catch (FileNotFoundException e) {
+            L.e("CacheHelper", "getObjectFromFile not exit... key:" + key);
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            QsHelper.getInstance().closeStream(fileInputStream, inputStreamReader, bufferedReader);
+        }
+        return null;
+    }
+
+    public <T extends QsModel> T getObjectFromFile(File cacheFile, Class<T> clazz) {
+        if (cacheFile == null || !cacheFile.exists()) {
+            return null;
+        }
+        FileInputStream fileInputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader bufferedReader = null;
+        try {
+            fileInputStream = new FileInputStream(cacheFile);
+            inputStreamReader = new InputStreamReader(fileInputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            String json = bufferedReader.readLine();
+            Gson gson = new Gson();
+            return gson.fromJson(json, clazz);
+        } catch (FileNotFoundException e) {
+            L.e("CacheHelper", "getObjectFromFile not exit... cacheFile:" + cacheFile.getPath());
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             QsHelper.getInstance().closeStream(fileInputStream, inputStreamReader, bufferedReader);
