7e6447ab14dde0ef6791b1292e9af61de0237e22
qsmaxmin <17600662013@163.com>
2018-08-24 16:30:02 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/mvp/QsABActivity.java
+++ base/src/main/java/com/qsmaxmin/qsbase/mvp/QsABActivity.java
@@ -51,7 +51,7 @@ public abstract class QsABActivity<P extends QsPresenter> extends AppCompatActiv
         return QsHelper.getInstance().getApplication().isLogOpen() ? getClass().getSimpleName() : "QsABActivity";
     }
 
-    @Override @ThreadPoint(ThreadType.MAIN) public void setActivityTitle(Object value, int code) {
+    @Override public void setActivityTitle(Object value, int code) {
 
     }
 
