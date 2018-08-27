7e85d76d39b4bc7c35cfd88417c2af43861316fb
qsmaxmin <17600662013@163.com>
2018-08-27 15:13:45 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/viewpager/QsViewPager.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/viewpager/QsViewPager.java
@@ -5,16 +5,19 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import com.qsmaxmin.qsbase.common.log.L;
+
 /**
  * @CreateBy QS
  * @Date 16/11/20  下午7:21
  * @Description 自定义viewpager
  */
 public class QsViewPager extends ViewPager {
-
-    public static final String TAG = "QsViewPager";
-
     public boolean canScroll = true;
+    float distanceX;
+    float distanceY;
+    float lastX;
+    float lastY;
 
     public QsViewPager(Context context) {
         this(context, null);
@@ -29,7 +32,7 @@ public class QsViewPager extends ViewPager {
     }
 
     @Override public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        return canScroll && super.onInterceptTouchEvent(arg0);
+        return canScroll && parseTouchEvent(arg0);
     }
 
     public void setCanScroll(boolean canScroll) {
@@ -38,5 +41,28 @@ public class QsViewPager extends ViewPager {
 
     public boolean isCanScroll() {
         return canScroll;
+    }
+
+    public boolean parseTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                distanceX = distanceY = 0f;
+                lastX = ev.getX();
+                lastY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float curX = ev.getX();
+                final float curY = ev.getY();
+                distanceX += Math.abs(curX - lastX);
+                distanceY += Math.abs(curY - lastY);
+                lastX = curX;
+                lastY = curY;
+                L.i("QsViewPager", "parseTouchEvent.........distanceX:" + distanceX + "  distanceY:" + distanceY + ", so " + (distanceX > distanceY ? "" : "not") + " intercept touch event");
+                if (distanceX > distanceY) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                    return true;
+                }
+        }
+        return super.onInterceptTouchEvent(ev);
     }
 }
