641c52c8052a707b4e70631b80d241ffb864249c
qsmaxmin <17600662013@163.com>
2018-10-15 11:45:21 +0800

    新增PhotoView控件，用于图片预览

---
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/photo/OnRectChangedListener.java
@@ -0,0 +1,10 @@
+package com.qsmaxmin.qsbase.common.widget.photo;
+
+/**
+ * @CreateBy qsmaxmin
+ * @Date 2018/10/15 10:59
+ * @Description
+ */
+public interface OnRectChangedListener {
+    void onChanged(float ratio, boolean complete);
+}
