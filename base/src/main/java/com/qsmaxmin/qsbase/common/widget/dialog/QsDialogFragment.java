25db7e2648f77d399406e45433e4cfc46124cfac
qsmaxmin <17600662013@163.com>
2018-12-12 15:59:14 +0800

    DialogTheme优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsDialogFragment.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsDialogFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.qsmaxmin.qsbase.R;
 import com.qsmaxmin.qsbase.common.utils.QsHelper;
@@ -32,13 +35,30 @@ public abstract class QsDialogFragment extends DialogFragment {
         return dialogView;
     }
 
+    @Override public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        final Window window = getDialog().getWindow();
+        if (window != null) {
+            window.getDecorView().setPadding(0, 0, 0, 0);
+            WindowManager.LayoutParams params = window.getAttributes();
+            setAttribute(params);
+            window.setAttributes(params);
+        }
+    }
+
+    protected void setAttribute(WindowManager.LayoutParams params) {
+        params.gravity = Gravity.BOTTOM;
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+    }
+
     @Override public void onStart() {
         super.onStart();
         initData();
     }
 
     protected int getDialogTheme() {
-        return R.style.QsDialogTheme;
+        return R.style.QsDialogTheme_Light;
     }
 
     protected abstract View getDialogView(LayoutInflater inflater, ViewGroup container);
