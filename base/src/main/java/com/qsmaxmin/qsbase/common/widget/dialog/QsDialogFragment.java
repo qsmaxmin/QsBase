359437a86ff9cc0652fa708423a040493b409141
qsmaxmin <17600662013@163.com>
2018-12-12 16:08:53 +0800

    DialogTheme优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsDialogFragment.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/dialog/QsDialogFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,9 +46,7 @@ public abstract class QsDialogFragment extends DialogFragment {
     }
 
     protected void setAttribute(WindowManager.LayoutParams params) {
-        params.gravity = Gravity.CENTER;
-        params.width = WindowManager.LayoutParams.WRAP_CONTENT;
-        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        // custom you attribute
     }
 
     @Override public void onStart() {
