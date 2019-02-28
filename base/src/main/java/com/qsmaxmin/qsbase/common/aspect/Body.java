9ce79861c7f3ff0cd97d16c3120cec3864c36b69
qsmaxmin <17600662013@163.com>
2019-02-28 10:52:01 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/aspect/Body.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/aspect/Body.java
@@ -10,7 +10,9 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 /**
  * @CreateBy qsmaxmin
  * @Date 2017/6/29 15:12
- * @Description
+ * @Description 请求体注解，POST等请求时将被加入到http请求体中，支持String, byte[], File, Object;
+ * 1，mimeType默认是JSON类型，所以当上传的格式非JSON时需要修改成对应的mimeType类型
+ * 2，其中Object类型默认解析成Json串并加入请求体中
  */
 @Documented
 @Target(PARAMETER)
