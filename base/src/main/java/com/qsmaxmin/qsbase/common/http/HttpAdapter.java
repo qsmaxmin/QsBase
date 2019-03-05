e6e10bb110d807754c2e2c9565359357f47066ef
qsmaxmin <17600662013@163.com>
2019-03-05 16:55:33 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpAdapter.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpAdapter.java
@@ -296,7 +296,7 @@ public class HttpAdapter {
         } catch (IOException e) {
             throw new QsException(QsExceptionType.HTTP_ERROR, requestTag, "IOException...  method:" + method.getName() + " message:" + e.getMessage());
         } catch (Throwable e) {
-            throw new QsException(QsExceptionType.UNEXPECTED, requestTag, "method:" + method.getName() + ", error:" + e.getMessage());
+            throw new QsException(QsExceptionType.NETWORK_ERROR, requestTag, "method:" + method.getName() + ", error:" + e.getMessage());
         }
     }
 
