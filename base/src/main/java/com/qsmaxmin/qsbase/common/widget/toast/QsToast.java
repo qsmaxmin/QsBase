cf16d32f4fb0a0c19eee752d82b75a175a9bce3a
程亮 <chengliang@grapedu.cn>
2020-03-19 13:58:38 +0800

    Toast优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/toast/QsToast.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/toast/QsToast.java
@@ -8,14 +8,29 @@ import android.widget.Toast;
 
 import com.qsmaxmin.qsbase.common.utils.QsHelper;
 
+import java.lang.ref.SoftReference;
+
 public class QsToast {
 
-    private static Toast mToast = null;
+    private static SoftReference<Toast> lastToast = null;
 
     /**
      * 简单Toast 消息弹出
      */
     public static void show(final String msg) {
+        show(msg, Toast.LENGTH_SHORT);
+    }
+
+
+    public static void show(@StringRes int resId) {
+        if (resId != 0) show(QsHelper.getString(resId));
+    }
+
+    public static void show(@StringRes int resId, int duration) {
+        if (resId != 0) show(QsHelper.getString(resId), duration);
+    }
+
+    public static void show(final String msg, int duration) {
         if (TextUtils.isEmpty(msg)) {
             return;
         }
@@ -27,13 +42,7 @@ public class QsToast {
                 }
             });
         } else {
-            showToast(QsHelper.getApplication(), msg, Toast.LENGTH_SHORT);
-        }
-    }
-
-    public static void show(@StringRes int resId) {
-        if (resId != 0) {
-            show(QsHelper.getString(resId));
+            showToast(QsHelper.getApplication(), msg, duration);
         }
     }
 
@@ -42,15 +51,24 @@ public class QsToast {
      */
     private static void showToast(Context context, String text, int duration) {
         try {
-            if (mToast == null) {
-                mToast = Toast.makeText(context, text, duration);
-                mToast.show();
+            int size = QsHelper.getScreenHelper().getActivityStack().size();
+            if (size > 0) {
+                cancelLastToast();
+                Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                toast.setDuration(duration);
+                toast.show();
+                lastToast = new SoftReference<>(toast);
             } else {
-                mToast.setText(text);
-                mToast.setDuration(duration);
-                mToast.show();
+                cancelLastToast();
             }
         } catch (Exception ignored) {
+        }
+    }
+
+    private static void cancelLastToast() {
+        if (lastToast != null) {
+            Toast toast = lastToast.get();
+            if (toast != null) toast.cancel();
         }
     }
 }
