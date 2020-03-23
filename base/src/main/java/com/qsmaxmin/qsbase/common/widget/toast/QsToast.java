581e05afd6c087152aa9215d9a605cd394d695ba
程亮 <chengliang@grapedu.cn>
2020-03-23 17:19:53 +0800

    QsToast优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/toast/QsToast.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/toast/QsToast.java
@@ -1,18 +1,66 @@
 package com.qsmaxmin.qsbase.common.widget.toast;
 
 import android.content.Context;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.StringRes;
 import android.text.TextUtils;
 import android.widget.Toast;
 
+import com.qsmaxmin.qsbase.common.log.L;
 import com.qsmaxmin.qsbase.common.utils.QsHelper;
 
 import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
 
 public class QsToast {
-
     private static SoftReference<Toast> lastToast = null;
+    private static Field                sField_TN;
+    private static Field                sField_TN_Handler;
+
+    static {
+        try {
+            sField_TN = Toast.class.getDeclaredField("mTN");
+            sField_TN.setAccessible(true);
+            sField_TN_Handler = sField_TN.getType().getDeclaredField("mHandler");
+            sField_TN_Handler.setAccessible(true);
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static void hook(Toast toast) {
+        try {
+            Object tn = sField_TN.get(toast);
+            Handler preHandler = (Handler) sField_TN_Handler.get(tn);
+            sField_TN_Handler.set(tn, new SafeHandler(preHandler));
+        } catch (Exception ignored) {
+        }
+    }
+
+    private static class SafeHandler extends Handler {
+        private Handler impl;
+
+        SafeHandler(Handler impl) {
+            this.impl = impl;
+        }
+
+        @Override
+        public void dispatchMessage(Message msg) {
+            try {
+                super.dispatchMessage(msg);
+            } catch (Exception e) {
+                if (L.isEnable()) L.e("QsToast", "hand exception..." + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            impl.handleMessage(msg);//需要委托给原Handler执行
+        }
+    }
+
 
     /**
      * 简单Toast 消息弹出
@@ -55,6 +103,7 @@ public class QsToast {
             if (size > 0) {
                 cancelLastToast();
                 Toast toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                hook(toast);
                 toast.setDuration(duration);
                 toast.show();
                 lastToast = new SoftReference<>(toast);
