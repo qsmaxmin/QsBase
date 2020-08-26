baace0daea3a630ae498c4c31151b214756d1ab7
qsmaxmin <871245760@qq.com>
2020-08-26 11:00:14 +0800

    v 10.0.0 developing

--- base/src/main/java/com/qsmaxmin/qsbase/plugin/property/QsProperties.java
+++ base/src/main/java/com/qsmaxmin/qsbase/plugin/property/QsProperties.java
@@ -68,94 +68,94 @@ public abstract class QsProperties implements QsIProperty, QsNotProguard {
     }
 
 
-    protected void putString(String key, String value) {
+    protected final void putString(String key, String value) {
         if (edit != null) edit.putString(key, value);
     }
 
-    protected void putInt(String key, int value) {
+    protected final void putInt(String key, int value) {
         if (edit != null) edit.putInt(key, value);
     }
 
-    protected void putInt2(String key, Integer value) {
+    protected final void putInt2(String key, Integer value) {
         if (edit != null) {
             edit.putInt(key, value == null ? 0 : value);
         }
     }
 
-    protected void putFloat(String key, float value) {
+    protected final void putFloat(String key, float value) {
         if (edit != null) edit.putFloat(key, value);
     }
 
-    protected void putFloat2(String key, Float value) {
+    protected final void putFloat2(String key, Float value) {
         if (edit != null) {
             edit.putFloat(key, value == null ? 0f : value);
         }
     }
 
-    protected void putLong(String key, long value) {
+    protected final void putLong(String key, long value) {
         if (edit != null) edit.putLong(key, value);
     }
 
-    protected void putLong2(String key, Long value) {
+    protected final void putLong2(String key, Long value) {
         if (edit != null) {
             edit.putLong(key, value == null ? 0L : value);
         }
     }
 
-    protected void putBoolean(String key, boolean value) {
+    protected final void putBoolean(String key, boolean value) {
         if (edit != null) edit.putBoolean(key, value);
     }
 
-    protected void putBoolean2(String key, Boolean value) {
+    protected final void putBoolean2(String key, Boolean value) {
         if (edit != null) edit.putBoolean(key, value == null ? false : value);
     }
 
-    protected void putStringSet(String key, Set<String> value) {
+    protected final void putStringSet(String key, Set<String> value) {
         if (edit != null) edit.putStringSet(key, value);
     }
 
-    protected int getInt(Map map, String key) {
+    protected final int getInt(Map map, String key) {
         Object o = map.get(key);
         return o == null ? 0 : (int) o;
     }
 
-    protected Integer getInt2(Map map, String key) {
+    protected final Integer getInt2(Map map, String key) {
         return (Integer) map.get(key);
     }
 
-    protected float getFloat(Map map, String key) {
+    protected final float getFloat(Map map, String key) {
         Object o = map.get(key);
         return o == null ? 0f : (float) o;
     }
 
-    protected Float getFloat2(Map map, String key) {
+    protected final Float getFloat2(Map map, String key) {
         return (Float) map.get(key);
     }
 
-    protected long getLong(Map map, String key) {
+    protected final long getLong(Map map, String key) {
         Object o = map.get(key);
         return o == null ? 0L : (long) o;
     }
 
-    protected Long getLong2(Map map, String key) {
+    protected final Long getLong2(Map map, String key) {
         return (Long) map.get(key);
     }
 
-    protected boolean getBoolean(Map map, String key) {
+    protected final boolean getBoolean(Map map, String key) {
         Object o = map.get(key);
         return o != null && (boolean) o;
     }
 
-    protected Boolean getBoolean2(Map map, String key) {
+    protected final Boolean getBoolean2(Map map, String key) {
         return (Boolean) map.get(key);
     }
 
-    protected String getString(Map map, String key) {
+    protected final String getString(Map map, String key) {
         return (String) map.get(key);
     }
 
     @SuppressWarnings("unchecked")
-    protected Set<String> getStringSet(Map map, String key) {
+    protected final Set<String> getStringSet(Map map, String key) {
         return (Set<String>) map.get(key);
     }
 }
