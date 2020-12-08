2edd6ca3c3d3e28781481dca26355eaf31d9e42b
qsmaxmin <871245760@qq.com>
2020-12-08 13:43:32 +0800

    gradle版本升级

--- base/src/main/java/com/qsmaxmin/qsbase/common/log/L.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/log/L.java
@@ -46,6 +46,18 @@ public final class L {
         println(Log.ERROR, tag, message);
     }
 
+    public static void e(String tag, Throwable t) {
+        if (enable) {
+            Log.e(tag, t.getMessage(), t);
+        }
+    }
+
+    public static void e(String tag, String msg, Throwable t) {
+        if (enable) {
+            Log.e(tag, msg, t);
+        }
+    }
+
     private static void println(int priority, String tag, String message) {
         if (enable && !TextUtils.isEmpty(message)) {
             Log.println(priority, tag, message);
