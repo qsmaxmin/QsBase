5493654b6fc15e087ab4fec4eb70c5b3b7ebb224
qsmaxmin <871245760@qq.com>
2021-04-19 12:36:49 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpRequest.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/http/HttpRequest.java
@@ -459,20 +459,13 @@ public final class HttpRequest {
         }
     }
 
-    public static String valueToString(@NonNull Object value) {
+    private String valueToString(Object value) {
+        return valueToString(gson, value);
+    }
+
+    public static String valueToString(Gson gson, @NonNull Object value) {
         if (value.getClass().isArray()) {
-            Object[] a = (Object[]) value;
-            int iMax = a.length - 1;
-            if (iMax == -1) return "[]";
-            StringBuilder b = new StringBuilder();
-            b.append('[');
-            for (int i = 0; ; i++) {
-                b.append(a[i]);
-                if (i == iMax) {
-                    return b.append(']').toString();
-                }
-                b.append(",");
-            }
+            return gson.toJson(value);
         } else {
             return String.valueOf(value);
         }
