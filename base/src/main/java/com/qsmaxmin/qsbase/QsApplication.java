6d3f157fececf76d9ad268485414a52033b206fd
程亮 <chengliang@grapedu.cn>
2021-04-24 00:22:28 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/QsApplication.java
+++ base/src/main/java/com/qsmaxmin/qsbase/QsApplication.java
@@ -1,6 +1,8 @@
 package com.qsmaxmin.qsbase;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
 
 import com.qsmaxmin.qsbase.common.http.HttpInterceptor;
 import com.qsmaxmin.qsbase.common.utils.ImageHelper;
@@ -50,5 +52,23 @@ public abstract class QsApplication extends Application implements QsIApplicatio
     }
 
     @Override public void onCommonLoadImage(ImageHelper.Builder builder) {
+    }
+
+    @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+    }
+
+    @Override public void onActivityStarted(Activity activity) {
+    }
+
+    @Override public void onActivityResumed(Activity activity) {
+    }
+
+    @Override public void onActivityPaused(Activity activity) {
+    }
+
+    @Override public void onActivityStopped(Activity activity) {
+    }
+
+    @Override public void onActivityDestroyed(Activity activity) {
     }
 }
