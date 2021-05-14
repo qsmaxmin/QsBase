21f9bffc2345518d5ecc74cd4268b347e0694936
qsmaxmin <871245760@qq.com>
2021-05-14 11:23:16 +0800

    代码优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/aspect/FormBody.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/aspect/FormBody.java
@@ -11,7 +11,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * @CreateBy qsmaxmin
  * @Date 2017/6/29 15:12
  * @Description form body，支持Map，一级Json字符串，一级Model实体对象
- * @deprecated
  */
 @Documented
 @Target(PARAMETER)
