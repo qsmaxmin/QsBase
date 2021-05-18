ca5044cd6bc09274791634ad1259e9ce7c96fe2b
qsmaxmin <871245760@qq.com>
2021-05-18 19:10:06 +0800

    FunctionImageView优化

--- base/src/main/java/com/qsmaxmin/qsbase/common/widget/image/ExecutorTransform.java
+++ base/src/main/java/com/qsmaxmin/qsbase/common/widget/image/ExecutorTransform.java
@@ -30,8 +30,11 @@ public class ExecutorTransform extends ExecutorBase {
     }
 
     @Override public void run() {
-        if (progress <= 1f) {
+        if (progress < 1f) {
             progress += stepValue;
+            if (progress > 1f) {
+                progress = 1f;
+            }
             float value = interpolator.getInterpolation(progress);
             transform(beginValues, endValues, value);
             invalidate();
